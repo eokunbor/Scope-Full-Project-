a917afff02bba817e98a15ef1ece8373d3727798
Elizabeth Okunbor <elizabethokunbor04@gmail.com>
2025-10-22 01:48:58 -0500

    Scope Auth+Loading +Splash

---
+++ Scope/app/(dashboard)/(home)/mainHome.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const mainHome = () => {
+  return (
+    <View>
+      <Text>mainHome</Text>
+    </View>
+  )
+}
+
+export default mainHome
+
+const styles = StyleSheet.create({})
--- Scope/lib/appwrite.js
+++ Scope/lib/appwrite.js
@@ -1,9 +1,15 @@
-import { Client, Account, Databases } from 'react-native-appwrite';
+import { Client, Account, Avatars, Databases, ID } from 'react-native-appwrite';
 
-const client = new Client()
-    .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT) // Your API Endpoint
-    .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID) // Your API Endpoint
-    .setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM)
-    .setName(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_NAME);// Your project ID
+const client = new Client();
+
+client
+  .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT)
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID)
+  .setPlatform('dev.eokunbor.scope');
 
 export const account = new Account(client);
+export const avatars = new Avatars(client);
+export const databases = new Databases(client);
+export { ID }; // Now ID is imported above, so we can export it
+
+export default client;
